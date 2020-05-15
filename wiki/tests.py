8388ecba5479ce38ffedcaaacef230ed67a672c1
Jarquevious <jarquevious@gmail.com>
2020-05-15 12:41:37 -0700

    testing not complete

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_detail_page(self):
+        """ Tests the slug generated when saving a Page. """
+        user = User.objects.create()
+        user.save()
