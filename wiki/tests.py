2b0307288321c06b3a1d33ff6eeb67d3d04ffae4
Jarquevious <jarquevious@gmail.com>
2020-05-15 13:04:06 -0700

    one test added

--- wiki/tests.py
+++ wiki/tests.py
@@ -3,5 +3,20 @@ from django.test import TestCase
 # Create your tests here.
 def test_detail_page(self):
         """ Tests the slug generated when saving a Page. """
+        # Create a user and save to the database
         user = User.objects.create()
         user.save()
+        
+        # Create a page and save to the database
+        page = Page(title="My Detail Test Page", content="details_test", author=user)
+        page.save()
+        
+        # Slug is generated matches with what we expect
+        slug = page.slug
+        response = self.client.get(f'/{slug}/')
+
+        self.assertEqual(response.status_code, 200)
+        
+        info = self.client.get('/')
+        self.assertContains(info, 'makewiki', html=True)
+        
